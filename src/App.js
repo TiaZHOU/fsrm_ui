76cc3bf9cc374803f38447ed6ca59279fc310652
terry <tiazhou@student.unimelb.edu.au>
2023-06-26 14:14:24 +0930

    code clean

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './App.css';
-import JsonDisplayIntoTable from './components/ShowTable.js';
-import Statistics from "./components/SearchTable";
 import RoleFilter from "./components/RoleFilter";
-import { HashRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/*<JsonDisplayIntoTable/>*/}
           <RoleFilter/>
       </header>
     </div>
