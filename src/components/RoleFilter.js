c22378162aad57f4e5a735fb615f256a2b0f5f3e
terry <tiazhou@student.unimelb.edu.au>
2023-07-01 15:02:13 +1000

    bug fix/ mobile friendly

--- src/components/RoleFilter.js
+++ src/components/RoleFilter.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import data from '../data/data.json';
-import ActorDetails from "./ShowTable";
+import ActorDetails from './ShowTable';
+import './RoleFilter.css';
+
 const RoleFilter = () => {
     const [selectedRole, setSelectedRole] = useState(null);
     const [filteredActors, setFilteredActors] = useState([]);
@@ -12,13 +14,13 @@ const RoleFilter = () => {
         data.forEach(show => {
             const cast = show.cast;
             Object.keys(cast).forEach(key => {
-                    rolesSet.add(key);
+                rolesSet.add(key);
             });
         });
         setRoles([...rolesSet]);
-    }, [ filteredActors]);
+    }, [filteredActors]);
 
-    const handleRoleClick = (role) => {
+    const handleRoleClick = role => {
         setSelectedRole(role);
         const actors = new Set();
         data.forEach(show => {
@@ -32,42 +34,44 @@ const RoleFilter = () => {
         setFilteredActors([...actors]);
     };
 
-    const handleActorClick = (actor) => {
+    const handleActorClick = actor => {
         setSelectedActor(actor);
-    }
+    };
 
     return (
-        <div>
-            <div>
+        <div className="role-filter-container">
+            <div className="header">
                 <h1>浮生若梦 演出报告</h1>
                 <h2>角色</h2>
-                {roles.map((role, index) => (
-                    <React.Fragment key={role}>
-                        <button onClick={() => handleRoleClick(role)}>
-                            {role}
-                        </button>
-                        <text> </text>
-                        {(index + 1) % 5 === 0 && <br />}
-                    </React.Fragment>
+            </div>
+            <div className="role-buttons-container">
+                {roles.map((role) => (
+                    <button key={role} onClick={() => handleRoleClick(role)}>
+                        {role}
+                    </button>
                 ))}
             </div>
-            <button onClick={()=>{
-                setSelectedRole();
-                setSelectedActor();
-                setRoles([]);
-                setFilteredActors([])}}>
-                Reset</button>
-            {selectedRole && <h3> {selectedRole} 演员列表（按上场顺序）:</h3>}
-            {filteredActors.map((actor, index) =>(
-                    <React.Fragment key={actor}>
-                        <button onClick={() => handleActorClick(actor)}>
-                            {actor}
-                        </button>
-                        <text> </text>
-                        {(index + 1) % 5 === 0 && <br />}
-                    </React.Fragment>
-                ) )}
-
+            <button
+                className="reset-button"
+                onClick={() => {
+                    setSelectedRole(null);
+                    setSelectedActor(null);
+                    setRoles([]);
+                    setFilteredActors([]);
+                }}
+            >
+                Reset
+            </button>
+            {selectedRole && (
+                <h3 className="actor-list-heading">{selectedRole} 演员列表（按上场顺序）:</h3>
+            )}
+            <div className="actor-buttons-container">
+                {filteredActors.map((actor) => (
+                    <button key={actor} onClick={() => handleActorClick(actor)}>
+                        {actor}
+                    </button>
+                ))}
+            </div>
             {selectedActor && <ActorDetails actor={selectedActor} data={data} />}
         </div>
     );
--- src/components/ShowTable.js
+++ src/components/ShowTable.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
-
-// Assuming data is imported from a local json file
-// import data from './data.json';
+import './ActorDetails.css';
 
 const ActorDetails = ({ actor, data }) => {
     const [details, setDetails] = useState({
@@ -13,7 +10,7 @@ const ActorDetails = ({ actor, data }) => {
         roleCounts: {},
         mostWorkedWith: '',
         servingCount: 0,
-        neverWorkedWith: []
+        neverWorkedWith: [],
     });
 
     useEffect(() => {
@@ -22,11 +19,11 @@ const ActorDetails = ({ actor, data }) => {
         const actorShows = data.filter(show => {
             let cast = Object.values(show.cast).includes(actor);
             let serving = Object.values(show.serving).includes(actor);
-            if(serving) servingCount[actor]++;
-            if(cast) {
+            if (serving) servingCount[actor]++;
+            if (cast) {
                 Object.keys(show.cast).forEach(role => {
-                    if(show.cast[role] === actor) {
-                        if(roleCounts[role]) roleCounts[role]++;
+                    if (show.cast[role] === actor) {
+                        if (roleCounts[role]) roleCounts[role]++;
                         else roleCounts[role] = 1;
                     }
                 });
@@ -37,38 +34,54 @@ const ActorDetails = ({ actor, data }) => {
         const actorsWorkedWith = {};
         actorShows.forEach(show => {
             Object.values(show.cast).forEach(name => {
-                    if(actorsWorkedWith[name]) actorsWorkedWith[name]++;
-                    else actorsWorkedWith[name] = 1;
+                if (actorsWorkedWith[name]) actorsWorkedWith[name]++;
+                else actorsWorkedWith[name] = 1;
             });
         });
 
         let mostWorkedWith = '';
         let maxShowsTogether = 0;
         Object.keys(actorsWorkedWith).forEach(name => {
-            if(actorsWorkedWith[name] >= maxShowsTogether) {
+            if (actorsWorkedWith[name] >= maxShowsTogether) {
                 maxShowsTogether = actorsWorkedWith[name];
                 mostWorkedWith = name;
             }
         });
 
         const allActors = new Set();
-        data.forEach(show => Object.values(show.cast).forEach(name => allActors.add(name)));
-        const neverWorkedWith = Array.from(allActors).filter(name => !actorsWorkedWith[name]);
+        data.forEach(show =>
+            Object.values(show.cast).forEach(name => allActors.add(name))
+        );
+        const neverWorkedWith = Array.from(allActors).filter(
+            name => !actorsWorkedWith[name]
+        );
+
+
+        const mostWorkedWithList = Object.entries(actorsWorkedWith)
+            .sort((a, b) => b[1] - a[1])
+            .map(([name, count]) => `${name} (${count} 场)`);
+
+        //get first 5 elements of mostWorkedWithList
+        mostWorkedWith = mostWorkedWithList.slice(2, 5).join('');
 
         setDetails({
             showCount: actorShows.length,
             firstDay: actorShows[0]?.date,
-            lastDay: actorShows[actorShows.length - 1]?.date,
-            daysBetween: Math.round((new Date(actorShows[actorShows.length - 1]?.date) - new Date(actorShows[0]?.date)) / (1000 * 60 * 60 * 24)),
+            lastDay: actorShows.reduce((prev, current) => (new Date(prev?.date) > new Date(current?.date) ? prev : current))?.date,
+            daysBetween: Math.round(
+                (new Date(actorShows[actorShows.length - 1]?.date) -
+                    new Date(actorShows[0]?.date)) /
+                (1000 * 60 * 60 * 24)
+            ),
             roleCounts,
             mostWorkedWith: mostWorkedWith,
             servingCount: servingCount[actor],
-            neverWorkedWith
+            neverWorkedWith,
         });
     }, [actor, data]);
 
     return (
-        <div>
+        <div className="actor-details-container">
             <h1>{actor}</h1>
             <h6>（统计来源为所有排班表中的正式场次，不包含排练、内测、跟场）</h6>
             <h2>在过去的 {data.length} 场演出中</h2>
@@ -79,21 +92,30 @@ const ActorDetails = ({ actor, data }) => {
             <h2>角色统计:</h2>
             <h6>-灵殒（徐虎，袁思道）与浮生若梦分开统计，商海角色和浮生若梦一起统计-</h6>
             <ul>
-                {Object.entries(details.roleCounts).map(([role, count]) => <li key={role}>{role}: {count} 场</li>)}
+                {Object.entries(details.roleCounts).map(([role, count]) => (
+                    <li key={role}>
+                        {role}: {count} 场
+                    </li>
+                ))}
             </ul>
-            {details.servingCount >0 && <li>前台: {details.servingCount} 场</li>}
+            {details.servingCount > 0 && (
+                <li>前台: {details.servingCount} 场</li>
+            )}
+            <h2>与你合作最多的演员:</h2>
+            <h2>
+                {details.mostWorkedWith}
+            </h2>
             <h2>很遗憾，有很多人还没有合作过:</h2>
             <ul>
-                {details.neverWorkedWith.map((actor,index) =>(
+                {details.neverWorkedWith.map((actor, index) => (
                     <React.Fragment key={actor}>
-                        <text key={actor}>{actor}  </text>
-                     {(index + 1) % 5 === 0 && <br />}
-                    </React.Fragment>)
-            )}
+                        <text key={actor}>{actor} </text>
+                        {(index + 1) % 5 === 0 && <br />}
+                    </React.Fragment>
+                ))}
             </ul>
-
         </div>
     );
-}
+};
 
 export default ActorDetails;
