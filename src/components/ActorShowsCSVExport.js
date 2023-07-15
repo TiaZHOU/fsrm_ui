ae8a1cbb151aac6838ef085213bc0041c6a64ca4
terry <tiazhou@student.unimelb.edu.au>
2023-07-15 15:02:47 +1000

    Revert "add pages jumper"
    This reverts commit daf3d33120d2509b706d41a9f40514a73765dbfb.

--- src/components/ActorShowsCSVExport.js
+++ src/components/ActorShowsCSVExport.js
@@ -31,7 +31,7 @@ const ActorShowsCSVExport = ({ actor, data }) => {
           headers={csvHeaders}
           filename={`${actor}_shows.csv`}
         >
-          Export data
+          Export CSV
         </CSVLink>
       </div>
     </>
